ed161c99297c8bc0c94d16b4c9bfb553f85ebe74
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-02-18 11:31:50 +0100

    fog

--- pages/Locations.py
+++ pages/Locations.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
-labels = 'Poland', 'USA', 'Europe' 
-sizes = [15, 30, 45]
-explode = (0, 0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+sizes = [15, 30, 45, 10]
+explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
