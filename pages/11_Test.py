44518907697ba97d9575c7a75c7870be0fa93afd
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-02-23 00:08:32 +0100

    dsd

--- pages/11_Test.py
+++ pages/11_Test.py
@@ -3,16 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def generate_data():
-    np.random.seed(0)
-    data = np.random.randn(10, 5).cumsum(axis=0)
-    df = pd.DataFrame(data, columns=['Risk 1', 'Risk 2', 'Risk 3', 'Risk 4', 'Risk 5'])
-    return df
+mydfdata = [('France', 30), ('Holland', 55), ('Germany',99), ('Others', 43)]
 
-def main():
-    st.title('Possible Risks Evolutions')
-    st.write('This graph shows the possible evolutions of different risks over time.')
-    
-    df = generate_data()
-    st.line_chart(df)
+mydf = pd.DataFrame(mydfdata, columns=['thing', 'count'])
 
+mydf
