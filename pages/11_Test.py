345324651ba332ec28c63e67321c4780e4e5dbb1
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-02-23 00:09:33 +0100

    sq

--- pages/11_Test.py
+++ pages/11_Test.py
@@ -8,3 +8,7 @@ mydfdata = [('France', 30), ('Holland', 55), ('Germany',99), ('Others', 43)]
 mydf = pd.DataFrame(mydfdata, columns=['thing', 'count'])
 
 mydf
+
+mydf.plot(kind='hist', bins=50,
+                     title='Distribution of View Count',
+                     figsize=(15, 5))
