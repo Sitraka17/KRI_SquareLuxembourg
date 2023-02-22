3c43a482d70995d12347fd9d1371e0e58c0d7258
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-02-23 00:04:41 +0100

    fruits

--- pages/11_Test.py
+++ pages/11_Test.py
@@ -1,21 +1,26 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
 def generate_data():
-    np.random.seed(0)
-    data = np.random.randn(10, 5).cumsum(axis=0)
-    df = pd.DataFrame(data, columns=['Risk 1', 'Risk 2', 'Risk 3', 'Risk 4', 'Risk 5'])
-    return df
+    data = {
+        'Apples': 10,
+        'Bananas': 20,
+        'Oranges': 30,
+        'Pears': 40,
+    }
+    return data
 
 def main():
-    st.title('Possible Risks Evolutions')
-    st.write('This graph shows the possible evolutions of different risks over time.')
-    
-    df = generate_data()
-    st.line_chart(df)
+    st.title('Fruit Count')
+    st.write('This pie chart shows the count of some fruits.')
+
+    data = generate_data()
+    labels = list(data.keys())
+    values = list(data.values())
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels, autopct='%1.1f%%')
+    st.pyplot(fig)
 
 if __name__ == '__main__':
     main()
-
