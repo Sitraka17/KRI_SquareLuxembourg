30a3df5a4a2c2df690845b646b78673ffaf7a163
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-02-22 13:42:08 +0100

    Magpie101

--- pages/Locations.py
+++ pages/Locations.py
@@ -17,8 +17,15 @@ st.set_page_config(page_title='ERB', page_icon=':bar_chart:', layout='wide')
 # Title
 st.title('Locations')
 
-import plotly.express as px
-df = px.data.gapminder().query("year == 2007").query("continent == 'Europe'")
-df.loc[df['pop'] < 2.e6, 'country'] = 'Other countries' # Represent only large countries
-fig = px.pie(df, values='pop', names='country', title='Population of European continent')
-fig.show()
+
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+sizes = [15, 30, 45, 10]
+explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+st.pyplot(fig1)
