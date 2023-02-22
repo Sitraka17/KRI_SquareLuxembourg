b51d7844cedd033c047ba85e7ee0eb4ad9a7f561
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-02-22 13:39:07 +0100

    Locations

--- pages/Locations.py
+++ pages/Locations.py
@@ -1,3 +1,22 @@
+# Libraries Block 
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.subplots as sp
+import data
+import numpy as np
+
+# Global Variables
+theme_plotly = None # None or streamlit
+week_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+# Config
+st.set_page_config(page_title='ERB', page_icon=':bar_chart:', layout='wide')
+
+# Title
+st.title('Locations')
+
 import plotly.express as px
 df = px.data.gapminder().query("year == 2007").query("continent == 'Europe'")
 df.loc[df['pop'] < 2.e6, 'country'] = 'Other countries' # Represent only large countries
