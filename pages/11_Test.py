ba7e4a296d1472253e2b629e05b3022268b736af
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-02-23 00:11:22 +0100

    pie

--- pages/11_Test.py
+++ pages/11_Test.py
@@ -3,12 +3,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-mydfdata = [('France', 30), ('Holland', 55), ('Germany',99), ('Others', 43)]
 
-mydf = pd.DataFrame(mydfdata, columns=['thing', 'count'])
+y = np.array([35, 25, 25, 15])
 
-mydf
+#plt.pie(y)
+#plt.show() 
 
-mydf.plot(kind='hist', bins=50,
-                     title='Distribution of View Count',
-                     figsize=(15, 5))
+st.pie_chart(y)
+
+#mydfdata = [('France', 30), ('Holland', 55), ('Germany',99), ('Others', 43)]
+#mydf = pd.DataFrame(mydfdata, columns=['thing', 'count'])
+#mydf
+#mydf.plot(kind='hist', bins=50,
+#                     title='Distribution of View Count',
+#                     figsize=(15, 5))
