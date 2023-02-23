ab5b79d50e365d2df8aa9425752b77b73abdb6b6
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-02-23 01:10:19 +0100

    TA MERE

--- pages/11_Test.py
+++ pages/11_Test.py
@@ -9,13 +9,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+import streamlit as st
+import pandas as pd
+import numpy as np
+
 df = pd.DataFrame(
-   np.random.randn(50, 20),
+   np.random.randn(10, 20),
    columns=('col %d' % i for i in range(20)))
+st.dataframe(df.style.highlight_max(axis=0))
 
-st.dataframe(df)  # Same as st.write(df)
 
+import pandas as pd
+data = {'Industry Code': ['A', 'C', 'D', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'Q'],
+        'Industry Description': ['Agriculture, Forestry And Fishing', 'Manufacturing', 'Electricity, Gas, Steam And Air Conditioning Supply', 'Construction', 'Wholesale And Retail Trade; Repair Of Motor Vehicles And.', 'Transportation And Storage', 'Information And Communication', 'Financial And insurance Activities', 'Real Estate Activities', 'Professional, Scientific And Technical Activities', 'Administrative And Support Service Activities', 'Human Health And']}
+df = pd.DataFrame(data)
 
+print(df)
 
 
 
